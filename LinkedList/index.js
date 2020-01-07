4bcf539d963c7d193ac107fc3bee8daec3b61b3d
Luc van Kerkvoort <luc.van.kerkvoort@gmail.com>
2020-01-07 13:29:04 -0800

    added the stack to the datastructures, added the reverse to the linkedlist

--- LinkedList/index.js
+++ LinkedList/index.js
@@ -1,9 +1,10 @@
 class Node {
   constructor(value) {
-    (this.value = value), (this.next = null);
+    (this.value = value), (this.next = null), (this.prev = null);
   }
 }
 
+// Singly LinkedList
 class LinkedList {
   constructor(value) {
     this.head = {
@@ -67,9 +68,81 @@ class LinkedList {
     leader.next = unwantedNode.next;
     this.length--;
   }
+  reverse() {
+    let first = this.head;
+    this.tail = this.head;
+    let second = first.next;
+    while (second) {
+      const temp = second.next;
+      second.next = first;
+      console.log(second.next);
+      first = second;
+      second = temp;
+    }
+    this.head.next = null;
+    this.head = first;
+    return this.printList();
+  }
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.prepend(15);
 myLinkedList.insert(2, 99);
+myLinkedList.printList();
+myLinkedList.reverse();
+
+class DoublyLinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+      prev: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = new Node(value);
+    newNode.prev = this.tail;
+    this.tail.next = newNode;
+    this.tail = newNode;
+    console.log(this.tail);
+    this.length++;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head.prev = newNode;
+    this.head = newNode;
+    this.length++;
+  }
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    let newNode = new Node(value);
+    let leader = this.traverseToIndex(index - 1);
+    const follower = leader.next;
+    leader.next = newNode;
+    newNode.prev = leader;
+    newNode.next = follower;
+    follower.prev = newNode;
+    this.length++;
+    console.log(this);
+  }
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+}
+
+// const myDoublyLinkedList = new DoublyLinkedList(10);
+// myDoublyLinkedList.append(40);
+// myDoublyLinkedList.insert(1, 55);
---
+++ Stack/index.js
@@ -0,0 +1,49 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.top;
+  }
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.bottom) {
+      this.bottom = newNode;
+      this.top = newNode;
+    } else {
+      newNode.next = this.top;
+      this.top = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.top) {
+      return null;
+    }
+    if (this.top === this.bottom) {
+      this.bottom = null;
+    }
+
+    let currentNode = this.top;
+    this.top = this.top.next;
+    this.length--;
+    return currentNode;
+  }
+}
+
+const myStack = new Stack();
+myStack.push(20);
+myStack.push(40);
+myStack.push(60);
+myStack.peek();
+myStack.pop();
