16afaa86910f1d03d1e2fae25c48f08279738bf9
Luc van Kerkvoort <luc.van.kerkvoort@gmail.com>
2020-01-21 20:25:32 -0800

    redid the recursion exercise

--- Recursion/index.js
+++ Recursion/index.js
@@ -1,59 +1,27 @@
-function findFactorialRecursive(number) {
-  if (number === 2) {
-    return 2;
-  }
-  return number * findFactorialRecursive(number - 1);
-}
-
-findFactorialRecursive(5);
 function findFactorialIterative(number) {
-  let answer = 0;
-  let num = 1;
-  for (let i = 1; i < number; i++) {
-    answer = num * (i + 1);
-    num = answer;
+  let answer = 1;
+  if (answer === 2) {
+    return answer;
+  }
+  for (let i = 2; i <= number; i++) {
+    answer = answer * i;
   }
   return console.log(answer);
 }
+findFactorialIterative(5);
 
-findFactorialIterative(4);
-// list sum of numbers
-
-let arr = [1, 5, 6, 8, 2, 12, 26, 3, 4];
-let index = 0;
-let answer = 0;
-function recursiveExercise(arr, index, answer) {
-  if (arr.length === 1) {
-    return arr[0];
-  } else {
-    answer = answer + arr[index];
-    console.log(answer);
-    if (index === arr.length - 1) {
-      return console.log(answer);
-    }
-    index++;
-    recursiveExercise(arr, index, answer);
+function findFactorialRecursive(number) {
+  if (number === 2) {
+    return number;
   }
+  return number * findFactorialRecursive(number - 1);
 }
+findFactorialRecursive(5);
 
-// recursiveExercise(arr, index, answer);
-
-function fibonacciRecursive(n) {
-  if (n < 2) {
-    return n;
+function findFib(number) {
+  if (number < 2) {
+    return number;
   }
-
-  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+  return findFib(number - 1) + findFib(number - 2);
 }
-
-// fibonacciRecursive(8);
-
-function fibonacciIterative(n) {
-  let arr = [0, 1];
-  for (let i = 2; i <= n; i++) {
-    arr.push(arr[i - 2] + arr[i - 1]);
-  }
-  return console.log(arr.pop());
-}
-
-fibonacciIterative(8);
+findFib(8);
